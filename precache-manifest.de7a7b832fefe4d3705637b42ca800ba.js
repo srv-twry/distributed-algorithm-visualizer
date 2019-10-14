581b4b75efafab74c545fcc162a45bc084a4c0b3
Saurav Tiwary <srv.twry@gmail.com>
2019-10-14 12:41:29 +0530

    Updates

---
+++ precache-manifest.de7a7b832fefe4d3705637b42ca800ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf89d186e6bdcec41475e7485a530ed",
+    "url": "/distributed-algorithm-visualizer/index.html"
+  },
+  {
+    "revision": "f08bbda1288852794b73",
+    "url": "/distributed-algorithm-visualizer/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "2a4fea2ee6e46733b843",
+    "url": "/distributed-algorithm-visualizer/static/css/main.44c8f474.chunk.css"
+  },
+  {
+    "revision": "f08bbda1288852794b73",
+    "url": "/distributed-algorithm-visualizer/static/js/2.07360011.chunk.js"
+  },
+  {
+    "revision": "2a4fea2ee6e46733b843",
+    "url": "/distributed-algorithm-visualizer/static/js/main.0cc40d8c.chunk.js"
+  },
+  {
+    "revision": "e917424898813b074ee6",
+    "url": "/distributed-algorithm-visualizer/static/js/runtime-main.99b87fda.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/distributed-algorithm-visualizer/static/media/logo.25bf045c.svg"
+  }
+]);
